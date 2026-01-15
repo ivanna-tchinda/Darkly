3e8d5881f270c16899f96b3d4a744bb7d307cb27
Ivanna TCHINDA <itchinda@f2r2s3.clusters.42paris.fr>
2026-01-15 18:17:01 +0100

    almost

--- script.py
+++ script.py
@@ -1,34 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
 url = "http://192.168.56.103/.hidden/"
+i = 0
 
 def url_join(path):
     new_url = url + path
     return new_url
 
-def get_hidden_file():
-    # print("Visite de", url)
+def get_hidden_file(new_url, path):
 
-    response =  requests.get(url)
-    # print("response:", response)
+    new_url += path
+    response =  requests.get(new_url)
+    print("get", new_url)
     soup = BeautifulSoup(response.content, 'html.parser')
 
     links = soup.find_all("a")
-    for link in links:
-        if(link.get("href") == '../'):
+    # print("get all links", links)
+    for x in range(len(links)):
+        global i
+        i = i + 1
+        print("into", links[x].get("href"), i)
+        if(links[x].get("href") == '../' ):
             continue
 
-        if(link.get("href") == 'README'):
-            new_url = url_join("README")
-            soup = BeautifulSoup(response.content, 'html.parser')
-            text = soup.find_all("p")
+        if(links[x].get("href") == 'README'):
+            new_url += "README"
+            response =  requests.get(new_url)
+            text = response.text
+            # print(text)
             if "flag" in text:
                 print("flag trouve:", flag)
                 return
+            else:
+                return
+        # print("entering a new file")
+        get_hidden_file(new_url, links[x].get("href"))
+
         
-    # print("Link:", link.get("href"), "Text:", link.string)
-    get_hidden_file()
 
-
-
-get_hidden_file()
+get_hidden_file(url, "")
